7c4b767dd7bf88a5e4d8f5e3350c322f6949c81a
Kathryn Wheeler <kiwheel@bu.edu>
2023-01-17 15:50:53 -0500

    trying

--- manuscriptCode_currentScript.R
+++ manuscriptCode_currentScript.R
@@ -1,89 +1,91 @@
-print("entered script")
+args = commandArgs(trailingOnly=TRUE)
+my_task_id=args[1]
+num_tasks=args[2]
+fnames <- 10:47
+my_fnames=fnames[seq((my_task_id+1),47,num_tasks)]
 
-source('sharedVariables.R')
-source('NEON_Data_DownloadAndProcess.R')
-
-n.cores <- 10
-registerDoParallel(cores=n.cores)
-
-print("loaded packages")
-dataName="NEON_SoilTemp"
-NEON_ID='DP1.00041.001'
-
-IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-foreach(s=seq_along(NEON_siteNames)[4:47]) %do% {
+for(s in fnames){
+  
+  source('sharedVariables.R')
+  source('NEON_Data_DownloadAndProcess.R')
+  
+  print("loaded packages")
+  dataName="NEON_SoilTemp"
+  NEON_ID='DP1.00041.001'
+  
+  IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+  
   print(s)
   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
     stackByTable(paste0(savePath,'/filesToStack',IDnum))
   }
+  
+  # if(!file.exists(paste0(dataPath,"/",dataName))){
+  #   dir.create(paste0(dataPath,"/",dataName))
+  # }
+  
+  # downloadNEONdata(dataName=dataName,NEON_ID=NEON_ID,andStack=FALSE,
+  #                  includedSeq=seq(46,47))
+  # dataName="NEON_Roots"
+  # NEON_ID='DP1.10067.001'
+  
+  # IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+  # for(s in seq_along(NEON_siteNames)[1:47]){
+  #   print(s)
+  #   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
+  #   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
+  #     stackByTable(paste0(savePath,'/filesToStack',IDnum))
+  #   }
+  # }
+  # 
+  # dataName="NEON_relativeHumidity"
+  # NEON_ID='DP1.00098.001'
+  # 
+  # IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+  # for(s in seq_along(NEON_siteNames)[1:47]){
+  #   print(s)
+  #   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
+  #   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
+  #     stackByTable(paste0(savePath,'/filesToStack',IDnum))
+  #   }
+  # }
+  # 
+  # dataName="NEON_plantFoliarTraits"
+  # NEON_ID="DP1.10026.001"
+  # 
+  # IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+  # for(s in seq_along(NEON_siteNames)[1:47]){
+  #   print(s)
+  #   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
+  #   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
+  #     stackByTable(paste0(savePath,'/filesToStack',IDnum))
+  #   }
+  # }
+  # # 
+  # # 
+  # dataName="NEON_litterfall" #No: 2, 7,8, 11, 17, 18, 20, 23, 25, 27, 38, 41, 45
+  # NEON_ID="DP1.10033.001"
+  # 
+  # IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+  # for(s in seq_along(NEON_siteNames)[1:47]){
+  #   print(s)
+  #   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
+  #   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
+  #     stackByTable(paste0(savePath,'/filesToStack',IDnum))
+  #   }
+  # }
+  # 
+  # 
+  # # dataName="NEON_canopyWaterIndices"
+  # # NEON_ID="DP3.30019.001"
+  
+  # if(!file.exists(paste0(dataPath,"/",dataName))){
+  #   dir.create(paste0(dataPath,"/",dataName))
+  # }
+  # 
+  # downloadNEONdata(dataName=dataName,NEON_ID=NEON_ID,andStack=FALSE,
+  #                  includedSeq=seq(46,47))
+  # print("downloaded data")
+  
 }
-
-# if(!file.exists(paste0(dataPath,"/",dataName))){
-#   dir.create(paste0(dataPath,"/",dataName))
-# }
-
-# downloadNEONdata(dataName=dataName,NEON_ID=NEON_ID,andStack=FALSE,
-#                  includedSeq=seq(46,47))
-# dataName="NEON_Roots"
-# NEON_ID='DP1.10067.001'
-
-# IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-# for(s in seq_along(NEON_siteNames)[1:47]){
-#   print(s)
-#   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
-#   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
-#     stackByTable(paste0(savePath,'/filesToStack',IDnum))
-#   }
-# }
-# 
-# dataName="NEON_relativeHumidity"
-# NEON_ID='DP1.00098.001'
-# 
-# IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-# for(s in seq_along(NEON_siteNames)[1:47]){
-#   print(s)
-#   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
-#   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
-#     stackByTable(paste0(savePath,'/filesToStack',IDnum))
-#   }
-# }
-# 
-# dataName="NEON_plantFoliarTraits"
-# NEON_ID="DP1.10026.001"
-# 
-# IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-# for(s in seq_along(NEON_siteNames)[1:47]){
-#   print(s)
-#   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
-#   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
-#     stackByTable(paste0(savePath,'/filesToStack',IDnum))
-#   }
-# }
-# # 
-# # 
-# dataName="NEON_litterfall" #No: 2, 7,8, 11, 17, 18, 20, 23, 25, 27, 38, 41, 45
-# NEON_ID="DP1.10033.001"
-# 
-# IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-# for(s in seq_along(NEON_siteNames)[1:47]){
-#   print(s)
-#   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
-#   if(file.exists(paste0(savePath,'/filesToStack',IDnum))){
-#     stackByTable(paste0(savePath,'/filesToStack',IDnum))
-#   }
-# }
-# 
-# 
-# # dataName="NEON_canopyWaterIndices"
-# # NEON_ID="DP3.30019.001"
-
-# if(!file.exists(paste0(dataPath,"/",dataName))){
-#   dir.create(paste0(dataPath,"/",dataName))
-# }
-# 
-# downloadNEONdata(dataName=dataName,NEON_ID=NEON_ID,andStack=FALSE,
-#                  includedSeq=seq(46,47))
-# print("downloaded data")
-
-
