c50cc0bcde8721ec745827a1086101e676a46567
Kathryn Wheeler <kiwheel@bu.edu>
2023-02-01 17:05:58 -0500

    typo

--- downloadERA5.R
+++ downloadERA5.R
@@ -28,7 +28,7 @@ var <- variables[["api_name"]][[1]] #Only include the first variable
 
 for(s in seq_along(NEON_siteNames)){
   siteID <- NEON_siteNames[s]
-  print(siteName)
+  print(siteID)
   lat <- siteData$field_latitude[siteData$field_site_id==siteName]
   lon <- siteData$field_longitude[siteData$field_site_id==siteName]
   area <- rep(round(c(lat, long) * 4) / 4, 2)
