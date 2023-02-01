3ce710af79de9fa1102526e8bb93154ab06777ed
Kathryn Wheeler <kiwheel@bu.edu>
2023-02-01 13:11:10 -0500

    updated

--- manuscriptCode_currentScript.R
+++ manuscriptCode_currentScript.R
@@ -9,23 +9,29 @@
 source('sharedVariables.R')
 source('NEON_Data_DownloadAndProcess.R')
 
-# dataName="NEON_RH"
-# NEON_ID='DP1.00098.001'
+dataName="NEON_RH"
+NEON_ID='DP1.00098.001'
 dataName="NEON_Windspeed"
 NEON_ID='DP1.00001.001'
 
-if(!file.exists(paste0(dataPath,"/",dataName))){
-  dir.create(paste0(dataPath,"/",dataName))
-}
-# selectColumns <- c('verticalPosition','PARMean','PARMinimum','PARMaximum','PARVariance','PARFinalQF')
-# inFileName <- "PARPAR_30min.csv"
-# varName <- "PARMean"
+selectColumns <- c('verticalPosition','RHMean','RHMinimum','RHMaximum','RHVariance','dewTempMean',
+                   'RHFinalQF','dewTempFinalQF')
+inFileName <- "RH_30min.csv"
+varName <- "RHMean"
+
+# 
+# if(!file.exists(paste0(dataPath,"/",dataName))){
+#   dir.create(paste0(dataPath,"/",dataName))
+# }
+
+combineNEONdata(dataName=dataName,NEON_ID=NEON_ID,selectColumns=selectColumns,
+                inFileName=inFileName,dataPath=dataPath)
 
 #downloadNEONdata(dataName=dataName,NEON_ID=NEON_ID,andStack = FALSE)
 
-IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
-for(s in seq_along(NEON_siteNames)){
-  print(s)
-  savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
-  stackByTable(paste0(savePath,'/filesToStack',IDnum))
-}
+# IDnum <- strsplit(NEON_ID,"[.]")[[1]][2]
+# for(s in seq_along(NEON_siteNames)){
+#   print(s)
+#   savePath <- paste0(dataPath,dataName,'/',NEON_siteNames[s])
+#   stackByTable(paste0(savePath,'/filesToStack',IDnum))
+# }
