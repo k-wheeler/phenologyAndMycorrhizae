9edc274ded0e5384e629b0f0dd4f230964a058ee
Kathryn Wheeler <kiwheel@bu.edu>
2023-02-14 16:25:58 -0500

    removed array

--- computeWeeklyMetData_Script.R
+++ computeWeeklyMetData_Script.R
@@ -1,14 +1,17 @@
-args = commandArgs(trailingOnly=TRUE)
-print(args)
-my_task_id=as.numeric(args[1])
-num_tasks=as.numeric(args[2])
+# args = commandArgs(trailingOnly=TRUE)
+# print(args)
+# my_task_id=as.numeric(args[1])
+# num_tasks=as.numeric(args[2])
 fnames <- 1:47
-my_fnames=fnames[seq((my_task_id+1),47,num_tasks)]
+my_fnames=fnames#[seq((my_task_id+1),47,num_tasks)]
+source('sharedVariables.R')
+source('NEON_Data_DownloadAndProcess.R')
+source('computeWeeklyMetData.R')
 
 for(s in my_fnames){
-  source('sharedVariables.R')
-  source('NEON_Data_DownloadAndProcess.R')
-  source('computeWeeklyMetData.R')
+  # source('sharedVariables.R')
+  # source('NEON_Data_DownloadAndProcess.R')
+  # source('computeWeeklyMetData.R')
   # funName="mean"
   # dataName="NEON_SingleAirTemperature"
   # computeWeeklyMetDataFiles(p=2,siteID=NEON_siteNames[s],dataName=dataName,
@@ -16,6 +19,9 @@ for(s in my_fnames){
   
   funName="sum"
   dataName="NEON_PrecipitationData"
-  computeWeeklyMetDataFiles(p=2,siteID=NEON_siteNames[s],dataName=dataName,
+  if(!file.exists(paste0(dataPath,dataName,"_computedWeeklyData_",funName,"_",NEON_siteNames[s],".csv"))){
+  print(NEON_siteNames[s])
+    computeWeeklyMetDataFiles(p=2,siteID=NEON_siteNames[s],dataName=dataName,
                             dataPath=dataPath,funName=funName,nWeeks=8)
+  }
 }
