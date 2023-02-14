5e0d21a83a6a533e79f9978e2bf4dc4693197e51
Kathryn Wheeler <kiwheel@bu.edu>
2023-02-14 15:00:25 -0500

    Fixed week precip error

--- computeWeeklyMetData.R
+++ computeWeeklyMetData.R
@@ -3,31 +3,32 @@ calculateWeeklyWeather <- function(X,phenoRow,dataName,dat){
                                                                      (as.Date(as.character(phenoRow[2]))-X*7),by="day"))
   if(dataName%in%c('NEON_SingleAirTemperature')){
     weekDat <- weekDat %>% group_by(verticalPosition) %>% summarise(GDD=calculateGDD(tempSingleMean),
-                                                                    CDD=calculateCDD(tempSingleMean)) %>%
-      mutate(week=X,siteID=phenoRow[1],date=phenoRow[2])
+                                                                    CDD=calculateCDD(tempSingleMean))
   }else if(dataName=="NEON_PrecipitationData"){
-    weekDat <- weekDat %>% summarise(sumPrecip=sum(precipBulk)) %>%
-      mutate(week=X,siteID=phenoRow[1],date=phenoRow[2])
+    weekDat <- weekDat %>% summarise(sumPrecip=sum(precipBulk)) 
   }
+  weekDat <- weekDat %>%
+    mutate(week=X,siteID=as.character(phenoRow[1]),date=as.character(phenoRow[2]))
   return(weekDat)
 }
 calculateAllWeeklyWeather <- function(X,dataName,dat,nWeeks){
   allWeekDatList <- lapply(X=(0:(nWeeks-1)),FUN=calculateWeeklyWeather,phenoRow=X,dataName=dataName,dat=dat)
   allWeeks <-rbindlist(allWeekDatList,fill=TRUE)
   if(dataName%in%c('NEON_SingleAirTemperature')){
-    allWeeks <- pivot_wider(allWeeks,names_from=c(1,4),values_from=2:3)
+    allWeeks <- pivot_wider(allWeeks,names_from=c(1,4),values_from=2:3,names_prefix="week")
   }else if(dataName=="NEON_PrecipitationData"){
-    allWeeks <- pivot_wider(allWeeks,names_from=week,values_from=sumPrecip)
+    allWeeks <- pivot_wider(allWeeks,names_from=week,values_from=sumPrecip,names_prefix="week")
   }
   return(allWeeks)
 }
 
 computeWeeklyMetDataFiles <- function(p,siteID,dataName,dataPath,funName,nWeeks){
   #p=2 #For only breaking leaf buds 
+  siteName <- siteID
   phenoDat <- read.csv(file=paste0(dataPath,'NEON_PhenologyObservations/NEON_PhenoObservationData_',gsub(" ","",NEON_phenophase_names[p]),'.csv'))   
-  phenoDat <- phenoDat %>% filter(phenophaseIntensity == mediumIntensity_phenophases[p]) %>%
+  phenoDat <- phenoDat %>% filter(phenophaseIntensity == mediumIntensity_phenophases[p],siteID==siteName) %>%
     dplyr::select(siteID,date) %>% unique()
-  
+
   metDat <- read.csv(paste0(dataPath,dataName,"Dailydata_",funName,".csv"))
   
   metList=apply(X=phenoDat,MARGIN=1,FUN=calculateAllWeeklyWeather,
