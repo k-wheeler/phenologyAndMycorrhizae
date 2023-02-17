21c6930eeff8339b12daafa49e8e1f08bd9322fc
k-wheeler <wheelerk@mit.edu>
2023-02-17 11:06:30 -0500

    merging

--- computeWeeklyMetData_Script.R
+++ computeWeeklyMetData_Script.R
@@ -1,17 +1,17 @@
-# args = commandArgs(trailingOnly=TRUE)
-# print(args)
-# my_task_id=as.numeric(args[1])
-# num_tasks=as.numeric(args[2])
+args = commandArgs(trailingOnly=TRUE)
+print(args)
+my_task_id=as.numeric(args[1])
+num_tasks=as.numeric(args[2])
 fnames <- 1:47
-my_fnames=fnames#[seq((my_task_id+1),47,num_tasks)]
-source('sharedVariables.R')
-source('NEON_Data_DownloadAndProcess.R')
-source('computeWeeklyMetData.R')
+my_fnames=fnames[seq((my_task_id+1),47,num_tasks)]
+#source('sharedVariables.R')
+#source('NEON_Data_DownloadAndProcess.R')
+#source('computeWeeklyMetData.R')
 
 for(s in my_fnames){
-  # source('sharedVariables.R')
-  # source('NEON_Data_DownloadAndProcess.R')
-  # source('computeWeeklyMetData.R')
+  source('sharedVariables.R')
+  source('NEON_Data_DownloadAndProcess.R')
+  source('computeWeeklyMetData.R')
   # funName="mean"
   # dataName="NEON_SingleAirTemperature"
   # computeWeeklyMetDataFiles(p=2,siteID=NEON_siteNames[s],dataName=dataName,
