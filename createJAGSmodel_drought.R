5cc8dd9419e16d50ce0593af66584c162facad94
Kathryn Wheeler <kiwheel@bu.edu>
2023-03-29 16:03:41 -0400

    updated JAGS model to have informed priors on CDDcrit

--- createJAGSmodel_drought.R
+++ createJAGSmodel_drought.R
@@ -4,44 +4,60 @@ source('sharedVariables.R')
 source('runMCMC_Model.R')
 set.seed(0)
 
-
 #Load and Organize Data ----
 p=3
 load(file=paste0("allCombinedNEONDat_",gsub(" ","",NEON_phenophase_names[p]),".RData")) #Loaded as allComDat
 allComDat$phenoStatus <- as.numeric(as.character(allComDat$phenoStatus))
-selectAllComDat <- allComDat %>% filter(Mycorrhizal.type%in%c("AM","EcM"),!is.na(soilMoisture))
+selectAllComDat <- allComDat %>% filter(!is.na(soilMoisture),siteID=="HARV")
 
 subSelectAllComDat <- selectAllComDat[sample.int(nrow(selectAllComDat),10000,replace=FALSE),]
+allSunlightTimes <- read.csv(file=paste0(dataPath,'NEON_sunlightTimes.csv'))
+siteMaxDaylength <- allSunlightTimes %>% group_by(siteID) %>% 
+  summarize(maxDayLength=max(daylength))
+subSelectAllComDat <- left_join(subSelectAllComDat,siteMaxDaylength,by="siteID") %>%
+  mutate(pRatio=daylength/maxDayLength)
 
-drought <- cbind(subSelectAllComDat$individualID,subSelectAllComDat$soilMoisture)
-colnames(drought) <- c("individualID","drought")
-drought=as.data.frame(drought) %>% group_by(individualID) %>% mutate(rowNum=row_number()) %>%
+subSelectAllComDat$CDDp <- subSelectAllComDat$CDD_closest * subSelectAllComDat$pRatio
+
+drought <- cbind(subSelectAllComDat$scientificName,subSelectAllComDat$soilMoisture)
+colnames(drought) <- c("scientificName","drought")
+drought=as.data.frame(drought) %>% group_by(scientificName) %>% mutate(rowNum=row_number()) %>%
   pivot_wider(names_from=rowNum,values_from=drought)
-drought$individualID <- NULL
+drought$scientificName <- NULL
 
-CDD <- cbind(subSelectAllComDat$individualID,subSelectAllComDat$CDD_10)
-colnames(CDD) <- c("individualID","CDD")
-CDD=as.data.frame(CDD) %>% group_by(individualID) %>% mutate(rowNum=row_number()) %>%
-  pivot_wider(names_from=rowNum,values_from=CDD)
-CDD$individualID <- NULL
+CDDp <- cbind(subSelectAllComDat$scientificName,subSelectAllComDat$CDDp)
+colnames(CDDp) <- c("scientificName","CDDp")
+CDDp=as.data.frame(CDDp) %>% group_by(scientificName) %>% mutate(rowNum=row_number()) %>%
+  pivot_wider(names_from=rowNum,values_from=CDDp)
+CDDp$scientificName <- NULL
 
-phenoStatus <- cbind(subSelectAllComDat$individualID,subSelectAllComDat$phenoStatus)
-colnames(phenoStatus) <- c("individualID","phenoStatus")
-numObs <- as.data.frame(phenoStatus) %>% group_by(individualID) %>% summarize(n=n())
+phenoStatus <- cbind(subSelectAllComDat$scientificName,subSelectAllComDat$phenoStatus)
+colnames(phenoStatus) <- c("scientificName","phenoStatus")
+numObs <- as.data.frame(phenoStatus) %>% group_by(scientificName) %>% summarize(n=n())
 
-phenoStatus=as.data.frame(phenoStatus) %>% group_by(individualID) %>% mutate(rowNum=row_number()) %>%
+phenoStatus=as.data.frame(phenoStatus) %>% group_by(scientificName) %>% mutate(rowNum=row_number()) %>%
   pivot_wider(names_from=rowNum,values_from=phenoStatus)
-numObs <- left_join(phenoStatus,numObs,by="individualID")
+numObs <- left_join(phenoStatus,numObs,by="scientificName")
 numObs <- numObs$n
 
-phenoStatus$individualID <- NULL
+phenoStatus$scientificName <- NULL
 
-JAGSdat <- list(drought=matrix(as.numeric(as.matrix(drought)),ncol=ncol(drought))[numObs>10,],
-                CDD=matrix(as.numeric(as.matrix(CDD)),ncol=ncol(CDD))[numObs>10,],
-                phenoStatus=matrix(as.numeric(as.matrix(phenoStatus)),ncol=ncol(phenoStatus))[numObs>10,],
-                N=length(numObs[numObs>10]),
-                numObs=numObs[numObs>10]
+JAGSdat <- list(drought=matrix(as.numeric(as.matrix(drought)),ncol=ncol(drought)),
+                CDDp=matrix(as.numeric(as.matrix(CDDp)),ncol=ncol(CDDp)),
+                phenoStatus=matrix(as.numeric(as.matrix(phenoStatus)),ncol=ncol(phenoStatus)),
+                N=length(numObs),
+                numObs=numObs
                 )
+
+yCritPriorValues <- read.csv('archettiYcritValues.csv')
+#plot(density(yCritPriorValues$Ycrit))
+priorMean=mean(yCritPriorValues$Ycrit)
+priorVar=var(yCritPriorValues$Ycrit)
+JAGSdat$CDD_mean_shape=priorMean**2/priorVar #k in JAGS
+JAGSdat$CDD_mean_scale=priorVar/priorMean #theta in JAGS
+JAGSdat$CDD_tau_lower <- 0
+JAGSdat$CDD_tau_upper <- 1/(priorVar*30) #inflated by a lot because of limited range of prior data
+
 
 # yesData <- selectAllComDat %>% filter(phenoStatus=="1",drought<1,CDD_10>0)
 # #yesData <- selectAllComDat %>% filter(phenoStatus=="1")
@@ -57,23 +73,26 @@ generalModel = "
 model {
 #### Process Model
 for(i in 1:N){
-#CDDCrit[i] ~ dnorm(CDDCrit_mean,CDDCrit_tau) #Random effect
-k[i] ~ dnorm(k_mean,k_tau) #Random effect
+CDDCrit[i] ~ dnorm(CDDCrit_mean,CDDCrit_tau) #Random effect
+#k[i] ~ dnorm(k_mean,k_tau) #Random effect
+
 for(j in 1:numObs[i]){    
-phenoProb[i,j] <- ifelse(CDD[i,j]>CDDCrit[i,j],p,(1-p)) #1 for senescence and 0 for no
+phenoProb[i,j] <- ifelse(CDDp[i,j]>CDDCrit[i],p,(1-p)) #1 for senescence and 0 for no
 phenoStatus[i,j] ~ dbern(phenoProb[i,j]) 
-CDDCrit[i,j] <- exp(k[i]*(drought[i,j]))
+#CDDCrit[i,j] <- exp(k[i]*(drought[i,j]))
 }
 }
 
 #### Priors
-k_mean ~ dunif(0.01,10)
-k_tau ~ dunif(0.1,50)
+#k_mean ~ dunif(0.01,10)
+#k_tau ~ dunif(0.1,50)
 #CDDmean ~ dunif(0,1000)
 #tau ~ dunif(0,0.01)
 #CDDCrit ~dunif(0,400)
-#CDDCrit_mean ~ dunif(0,400)
-#CDDCrit_tau ~ dunif(0,0.00001)
+
+CDDCrit_mean ~ dgamma(CDD_mean_shape,CDD_mean_scale)
+CDDCrit_tau ~ dunif(CDD_tau_lower,CDD_tau_upper)
+
 p ~ dbeta(10,1)
 }
 "
@@ -85,33 +104,33 @@ j.model   <- jags.model(file = textConnection(generalModel),
                         n.chains = nchain)
 #variableNames <- c("p.proc","b0","b1","x")
 #variableNames <- c("CDDCrit_mean","CDDCrit_tau","p")
-variableNames <- c("k_mean","k_tau","k","p")
+variableNames <- c("CDDCrit","CDDCrit_mean","CDDCrit_tau","p")
 # out.burn <- coda.samples(model=j.model,variable.names=variableNames,n.iter=20000)
 out.burn <- runMCMC_Model(j.model=j.model,variableNames=variableNames,
-                          baseNum = 50000,iterSize = 10000)
-save(out.burn,file="SoilMoisture_JAGS_varBurn.RData")
+                          baseNum = 10000,iterSize = 5000)
+save(out.burn,file="DM_HARV_JAGS_varBurn.RData")
 out.mat <- as.data.frame(as.matrix(out.burn))
-
-pdf(file="test.pdf",
-    width=6,height=6)
-plot(density(out.mat$k_mean))
-plot(density(out.mat$k_tau))
-plot(density(out.mat$p))
-plot(density(out.mat$'k[1]'))
-plot(density(out.mat$'k[10]'))
-plot(density(out.mat$'k[20]'))
-plot(density(out.mat$'k[30]'))
-plot(density(out.mat$'k[40]'))
-plot(density(out.mat$'k[51]'))
-plot(density(out.mat$'k[61]'))
-plot(density(out.mat$'k[81]'))
-plot(density(out.mat$'k[101]'))
-plot(density(out.mat$'k[121]'))
-plot(density(out.mat$'k[141]'))
-plot(density(out.mat$'k[161]'))
-plot(density(out.mat$'k[181]'))
-plot(density(out.mat$'k[201]'))
-plot(density(out.mat$'k[241]'))
-plot(density(out.mat$'k[261]'))
-plot(density(out.mat$'k[301]'))
-dev.off()
+# 
+# pdf(file="test.pdf",
+#     width=6,height=6)
+# plot(density(out.mat$k_mean))
+# plot(density(out.mat$k_tau))
+# plot(density(out.mat$p))
+# plot(density(out.mat$'k[1]'))
+# plot(density(out.mat$'k[10]'))
+# plot(density(out.mat$'k[20]'))
+# plot(density(out.mat$'k[30]'))
+# plot(density(out.mat$'k[40]'))
+# plot(density(out.mat$'k[51]'))
+# plot(density(out.mat$'k[61]'))
+# plot(density(out.mat$'k[81]'))
+# plot(density(out.mat$'k[101]'))
+# plot(density(out.mat$'k[121]'))
+# plot(density(out.mat$'k[141]'))
+# plot(density(out.mat$'k[161]'))
+# plot(density(out.mat$'k[181]'))
+# plot(density(out.mat$'k[201]'))
+# plot(density(out.mat$'k[241]'))
+# plot(density(out.mat$'k[261]'))
+# plot(density(out.mat$'k[301]'))
+# dev.off()
