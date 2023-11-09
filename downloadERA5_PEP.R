0baa0cc66b0c9ce26b347e5def8a19dafd968cae
Kathryn Wheeler <kiwheel@bu.edu>
2023-11-09 15:20:53 -0500

    decreasing era5 variables to download

--- downloadERA5_PEP.R
+++ downloadERA5_PEP.R
@@ -12,21 +12,6 @@ cclient <- cdsapi$Client()
 variables <- tibble::tribble(
   ~cf_name, ~units, ~api_name, ~ncdf_name,
   "air_temperature", "Kelvin", "2m_temperature", "t2m",
-  "air_pressure", "Pa", "surface_pressure", NA_character_,
-  NA_character_, "Kelvin", "2m_dewpoint_temperature", NA_character_,
-  "precipitation_flux", "kg/m2/s", "total_precipitation", NA_character_,
-  "eastward_wind", "m/s", "10m_u_component_of_wind", NA_character_,
-  "northward_wind", "m/s", "10m_v_component_of_wind", NA_character_,
-  "surface_downwelling_shortwave_flux_in_air", "W/m2", "surface_solar_radiation_downwards", NA_character_,
-  "surface_downwelling_longwave_flux_in_air", "W/m2", "surface_thermal_radiation_downwards", NA_character_,
-  "soil_temperature_level_1","Kelvin","soil_temperature_level_1",NA_character_,
-  "soil_temperature_level_2","Kelvin","soil_temperature_level_2",NA_character_,
-  "soil_temperature_level_3","Kelvin","soil_temperature_level_3",NA_character_,
-  "soil_temperature_level_4","Kelvin","soil_temperature_level_4",NA_character_,
-  'volumetric_soil_water_layer_1','m3/m3','volumetric_soil_water_layer_1',NA_character_,
-  'volumetric_soil_water_layer_2','m3/m3','volumetric_soil_water_layer_2',NA_character_,
-  'volumetric_soil_water_layer_3','m3/m3','volumetric_soil_water_layer_3',NA_character_,
-  'volumetric_soil_water_layer_4','m3/m3','volumetric_soil_water_layer_4',NA_character_
 ) #Some examples (see more at: https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview)
 
 var <- variables[["api_name"]]
@@ -35,8 +20,8 @@ minLat <- min(includedStations$LAT) - 0.5
 maxLon <- max(includedStations$LON) + 0.5
 minLon <- min(includedStations$LON) - 0.5
 
-#area <- c(maxLat,minLon,minLat,minLon)
-area <- c(52,8,48,12)
+area <- c(maxLat,minLon,minLat,minLon)
+#area <- c(52,8,48,12)
 
 #for(s in (1:nrow(includedStations))){
 #lapply((1950:2022),function(X){
@@ -46,7 +31,7 @@ X <- 1980
   start_date <- as.Date(paste0(yr,"-01-01"))
   end_date <- as.Date(paste0(yr,"-12-31"))
   
-  fileName <- paste0(dataPath,'ERA5_PEP/',"PEPsites_",start_date,"_",end_date,"_era5MembersTEST.nc")
+  fileName <- paste0(dataPath,'ERA5_PEP/',"PEPsites_",start_date,"_",end_date,"_era5Members.nc")
   print(fileName)
   if(!file.exists(fileName)){
     do_next <- tryCatch({
