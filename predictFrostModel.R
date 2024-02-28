c48c5564b0198a14107eb049fde8621c28c5f2fa
Kathryn Wheeler <kiwheel@bu.edu>
2024-02-28 10:08:04 -0500

    Cleaned up

--- predictFrostModel.R
+++ predictFrostModel.R
@@ -5,172 +5,183 @@ library('rjags')
 library('runjags')
 library('tidyverse')
 
-#ncFiles <- paste0("HARV_ERA5/",dir(path="HARV_ERA5",pattern="nc"))
-ncFiles <- paste0('Data/ERA5_phenoObs/',dir(path='Data/ERA5_phenoObs/',pattern="nc"))
-phenoSites <- read.csv('allPhenoSites.csv')
-phenoSites$roundedLat <- round(c(phenoSites$latitude) * 4) / 4
-phenoSites$roundedLon <- round(c(phenoSites$longitude) * 4) / 4
-groupedSites <- phenoSites %>% group_by(roundedLat,roundedLon) %>% summarise(n=n())
-groupedSites$n <- NULL
-colnames(groupedSites) <- c('latitude','longitude')
-phenoSites <- groupedSites
-phenoSites <- phenoSites %>% filter(latitude<70 & latitude>35 & longitude < 42 & longitude > (-15))
-
-allCDD_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
-allGDD_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
-allDayLengths_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
-allCumP_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
-allFrostStatus_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
-
-allCDD_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
-allGDD_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
-allDayLengths_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
-allCumP_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
-allFrostStatus_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
-
-f=1
-ct <- 1
-#for(f in seq_along(ncFiles)){
-for(f in 1:50){
-  print(f)
-  nc <- nc_open(ncFiles[f])
-  
-  time <- as.integer(ncdf4::ncvar_get(nc, "time"))
-  time <- as.POSIXct(time*3600, origin = "1900-01-01",tz = "GMT")
-  time <- as.Date(time)
-  
-  latDim <- (ncdf4::ncvar_get(nc, "latitude"))
-  lonDim <- (ncdf4::ncvar_get(nc, "longitude"))
-  s=1
-  #for(s in 1:3){
-  for(s in 1:nrow(phenoSites)){
-    print(s/nrow(phenoSites))
-    lon=phenoSites$longitude[s]
-    lat=phenoSites$latitude[s]
-    print(c(lon,lat))
-    v <- "tp"
-    dat <- ncvar_get(nc,varid=v)
-    
-    dat <- dat[which(lonDim==lon),which(latDim==lat),,]
-    
-    dat <- colMeans(dat)
-    datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
-      summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
-      pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
-      mutate(var=v)
-    
-    dates <- unique(datTime$date)
-    datTime$cumP <- NA
-    datTime <- datTime %>% filter(var=="tp", funName=="sum")
-    cumP <- rep(NA,nrow(datTime))
-    cumP[1] <- datTime$value[1]
-    for(t in 2:nrow(datTime)){
-      cumP[t] <- cumP[(t-1)]+datTime$value[t]
-    }
-    
-    v <- 't2m'
-    dat <- ncvar_get(nc,varid=v)
-    dat <- dat[which(lonDim==lon),which(latDim==lat),,]
-    
-    dat <- colMeans(dat)
-    datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
-      summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
-      pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
-      mutate(var=v)
-    
-    datTime <- datTime %>% filter(funName=="mean")
-    datTime$value <- datTime$value - 273 #Convert from K to C
-    datTime$GDD_offset <- unlist(lapply(datTime$value,function(X){
-      return(max(X-0,0))
-    }))
-    
-    GDD <- rep(NA,nrow(datTime))
-    GDD[1:45] <- NA
-    
-    for(t in 46:nrow(datTime)){
-      GDD[t] <- sum(datTime$GDD_offset[(t-45):(t-1)])
-    }
-    
-    datTime$CDD_offset <- unlist(lapply(datTime$value,function(X){
-      return(max(20-X,0))
-    }))
-    
-    CDD <- rep(NA,nrow(datTime))
-    CDD[1:14] <- NA
-    
-    for(t in 15:nrow(datTime)){
-      CDD[t] <- sum(datTime$CDD_offset[(t-14):(t-1)])
-    }
-    
-    datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
-      summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
-      pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
-      mutate(var=v)
-    
-    datTime <- datTime %>% filter(funName=="min")
-    datTime$value <- datTime$value - 273
-    frostStatus <- rep(NA,length(dates))
-    
-    lastSpring <- which(datTime$value[1:182]<0)[length(which(datTime$value[1:182]<0))]
-    if(length(lastSpring)==0){
-      frostStatus[1:181] <- 1
-    }else{
-      frostStatus[1:(lastSpring)] <- 0
-      frostStatus[(lastSpring+1):181] <- 1
-    }
-    
-    firstAutumn <- which(datTime$value[182:length(datTime$value)]<0)[1]+181
-    if(is.na(firstAutumn)){
-      frostStatus[182:length(frostStatus)] <- 1
-    }else{
-      frostStatus[182:(firstAutumn-1)] <- 0
-      frostStatus[firstAutumn:length(frostStatus)] <- 1
-    }
-
-
-    
-    dayLengths <- numeric()
-    #lat <- 42.5378 
-    #lon <-  -72.1715
-    for(d in 1:length(dates)){
-      suntimes <- getSunlightTimes(date=dates[d],
-                                   lat=lat,lon=lon,keep=c("sunrise","sunset"),
-                                   tz = "GMT") #GMT because I only care about difference
-      dayLengths <- c(dayLengths,as.numeric(suntimes$sunset-suntimes$sunrise))
-    }
-    yr <- lubridate::year(dates[1])
-    allCDD_spring[ct,1:185] <- c(lat,lon,yr,CDD[1:182])
-    allGDD_spring[ct,1:185] <- c(lat,lon,yr,GDD[1:182])
-    allDayLengths_spring[ct,1:185] <- c(lat,lon,yr,dayLengths[1:182])
-    allCumP_spring[ct,1:185] <- c(lat,lon,yr,cumP[1:182])
-    allFrostStatus_spring[ct,1:185] <- c(lat,lon,yr,frostStatus[1:182])
-    
-    allCDD_fall[ct,1:186] <- c(lat,lon,yr,CDD[183:365])
-    allGDD_fall[ct,1:186] <- c(lat,lon,yr,GDD[183:365])
-    allDayLengths_fall[ct,1:186] <- c(lat,lon,yr,dayLengths[183:365])
-    allCumP_fall[ct,1:186] <- c(lat,lon,yr,cumP[183:365])
-    allFrostStatus_fall[ct,1:186] <- c(lat,lon,yr,frostStatus[183:365])
-    ct <- ct + 1
+for(continent in c('europe','USA')){
+  continent <- "europe" #Or "europe" or "USA"
+  if(continent=="europe"){
+    maxLat <- 70
+    minLat <- 35
+    maxLon <- 42
+    minLon <- -15
+  }else if(continent=="USA"){
+    maxLat <- 51
+    minLat <- 25
+    maxLon <- (-63)
+    minLon <- (-130)
   }
-  nc_close(nc)
   
+  
+  ncFiles <- paste0('Data/ERA5_phenoObs/',dir(path='Data/ERA5_phenoObs/',pattern=continent))
+  phenoSites <- read.csv('allPhenoSites.csv')
+  phenoSites$roundedLat <- round(c(phenoSites$latitude) * 4) / 4
+  phenoSites$roundedLon <- round(c(phenoSites$longitude) * 4) / 4
+  groupedSites <- phenoSites %>% group_by(roundedLat,roundedLon) %>% summarise(n=n())
+  groupedSites$n <- NULL
+  colnames(groupedSites) <- c('latitude','longitude')
+  phenoSites <- groupedSites
+  phenoSites <- phenoSites %>% filter(latitude<maxLat & latitude>minLat & longitude < maxLon & longitude > minLon)
+  
+  allCDD_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
+  allGDD_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
+  allDayLengths_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
+  allCumP_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
+  allFrostStatus_spring <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=185)
+  
+  allCDD_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
+  allGDD_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
+  allDayLengths_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
+  allCumP_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
+  allFrostStatus_fall <- matrix(nrow=nrow(phenoSites)*length(ncFiles),ncol=186)
+  
+  f=1
+  ct <- 1
+  for(f in seq_along(ncFiles)){
+    #for(f in 1:50){
+    print(f)
+    nc <- nc_open(ncFiles[f])
+    
+    time <- as.integer(ncdf4::ncvar_get(nc, "time"))
+    time <- as.POSIXct(time*3600, origin = "1900-01-01",tz = "GMT")
+    time <- as.Date(time)
+    
+    latDim <- (ncdf4::ncvar_get(nc, "latitude"))
+    lonDim <- (ncdf4::ncvar_get(nc, "longitude"))
+    s=1
+    #for(s in 1:3){
+    for(s in 1:nrow(phenoSites)){
+      print(s/nrow(phenoSites))
+      lon=phenoSites$longitude[s]
+      lat=phenoSites$latitude[s]
+      print(c(lon,lat))
+      v <- "tp"
+      dat <- ncvar_get(nc,varid=v)
+      
+      dat <- dat[which(lonDim==lon),which(latDim==lat),,]
+      
+      dat <- colMeans(dat,na.rm=TRUE)
+      datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
+        summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
+        pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
+        mutate(var=v)
+      
+      dates <- unique(datTime$date)
+      datTime$cumP <- NA
+      datTime <- datTime %>% filter(var=="tp", funName=="sum")
+      cumP <- rep(NA,nrow(datTime))
+      cumP[1] <- datTime$value[1]
+      for(t in 2:nrow(datTime)){
+        cumP[t] <- cumP[(t-1)]+datTime$value[t]
+      }
+      
+      v <- 't2m'
+      dat <- ncvar_get(nc,varid=v)
+      dat <- dat[which(lonDim==lon),which(latDim==lat),,]
+      
+      dat <- colMeans(dat)
+      datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
+        summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
+        pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
+        mutate(var=v)
+      
+      datTime <- datTime %>% filter(funName=="mean")
+      datTime$value <- datTime$value - 273 #Convert from K to C
+      datTime$GDD_offset <- unlist(lapply(datTime$value,function(X){
+        return(max(X-0,0))
+      }))
+      
+      GDD <- rep(NA,nrow(datTime))
+      GDD[1:45] <- NA
+      
+      for(t in 46:nrow(datTime)){
+        GDD[t] <- sum(datTime$GDD_offset[(t-45):(t-1)])
+      }
+      
+      datTime$CDD_offset <- unlist(lapply(datTime$value,function(X){
+        return(max(20-X,0))
+      }))
+      
+      CDD <- rep(NA,nrow(datTime))
+      CDD[1:14] <- NA
+      
+      for(t in 15:nrow(datTime)){
+        CDD[t] <- sum(datTime$CDD_offset[(t-14):(t-1)])
+      }
+      
+      datTime <- data.frame(var=dat,date=time) %>% group_by(date) %>% 
+        summarise(mean=mean(var),min=min(var),max=max(var),sum=sum(var)) %>%
+        pivot_longer(cols=2:5,names_to="funName",values_to="value") %>%
+        mutate(var=v)
+      
+      datTime <- datTime %>% filter(funName=="min")
+      datTime$value <- datTime$value - 273
+      frostStatus <- rep(NA,length(dates))
+      
+      lastSpring <- which(datTime$value[1:182]<0)[length(which(datTime$value[1:182]<0))]
+      if(length(lastSpring)==0){
+        frostStatus[1:181] <- 1
+      }else{
+        frostStatus[1:(lastSpring)] <- 0
+        frostStatus[(lastSpring+1):181] <- 1
+      }
+      
+      firstAutumn <- which(datTime$value[182:length(datTime$value)]<0)[1]+181
+      if(is.na(firstAutumn)){
+        frostStatus[182:length(frostStatus)] <- 1
+      }else{
+        frostStatus[182:(firstAutumn-1)] <- 0
+        frostStatus[firstAutumn:length(frostStatus)] <- 1
+      }
+      
+      dayLengths <- numeric()
+      for(d in 1:length(dates)){
+        suntimes <- getSunlightTimes(date=dates[d],
+                                     lat=lat,lon=lon,keep=c("sunrise","sunset"),
+                                     tz = "GMT") #GMT because I only care about difference
+        dayLengths <- c(dayLengths,as.numeric(suntimes$sunset-suntimes$sunrise))
+      }
+      yr <- lubridate::year(dates[1])
+      allCDD_spring[ct,1:185] <- c(lat,lon,yr,CDD[1:182])
+      allGDD_spring[ct,1:185] <- c(lat,lon,yr,GDD[1:182])
+      allDayLengths_spring[ct,1:185] <- c(lat,lon,yr,dayLengths[1:182])
+      allCumP_spring[ct,1:185] <- c(lat,lon,yr,cumP[1:182])
+      allFrostStatus_spring[ct,1:185] <- c(lat,lon,yr,frostStatus[1:182])
+      
+      allCDD_fall[ct,1:186] <- c(lat,lon,yr,CDD[183:365])
+      allGDD_fall[ct,1:186] <- c(lat,lon,yr,GDD[183:365])
+      allDayLengths_fall[ct,1:186] <- c(lat,lon,yr,dayLengths[183:365])
+      allCumP_fall[ct,1:186] <- c(lat,lon,yr,cumP[183:365])
+      allFrostStatus_fall[ct,1:186] <- c(lat,lon,yr,frostStatus[183:365])
+      ct <- ct + 1
+    }
+    nc_close(nc)
+    
+  }
+  
+  
+  write.csv(allCDD_spring,file=paste0("allCDD_spring_",continent,".csv"))
+  write.csv(allGDD_spring,file=paste0("allGDD_spring_",continent,".csv"))
+  
+  write.csv(allCDD_fall,file=paste0("allCDD_fall_",continent,".csv"))
+  write.csv(allGDD_fall,file=paste0("allGDD_fall_",continent,".csv"))
+  
+  write.csv(allDayLengths_spring,file=paste0("allDayLengths_spring_",continent,".csv"))
+  write.csv(allDayLengths_fall,file=paste0("allDayLengths_fall_",continent,".csv"))
+  
+  write.csv(allCumP_spring,file=paste0("allCumP_spring_",continent,".csv"))
+  write.csv(allCumP_fall,file=paste0("allCumP_fall_",continent,".csv"))
+  
+  write.csv(allFrostStatus_spring,file=paste0("allFrostStatus_spring_",continent,".csv"))
+  write.csv(allFrostStatus_fall,file=paste0("allFrostStatus_fall_",continent,".csv"))
 }
-
-
-write.csv(allCDD_spring,file="allCDD_spring_europe.csv")
-write.csv(allGDD_spring,file="allGDD_spring_europe.csv")
-
-write.csv(allCDD_fall,file="allCDD_fall_europe.csv")
-write.csv(allGDD_fall,file="allGDD_fall_europe.csv")
-
-write.csv(allDayLengths_spring,file="allDayLengths_spring_europe.csv")
-write.csv(allDayLengths_fall,file="allDayLengths_fall_europe.csv")
-
-write.csv(allCumP_spring,file="allCumP_spring_europe.csv")
-write.csv(allCumP_fall,file="allCumP_fall_europe.csv")
-
-write.csv(allFrostStatus_spring,file="allFrostStatus_spring_europe.csv")
-write.csv(allFrostStatus_fall,file="allFrostStatus_fall_europe.csv")
 
 # 
 # 
