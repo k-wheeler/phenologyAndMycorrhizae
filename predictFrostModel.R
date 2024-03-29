4182241978221a04b057ba36995bdd30b838ceac
Kathryn Wheeler <kiwheel@bu.edu>
2024-03-29 13:16:41 -0400

    Added code to plot model outputs and generalized for fall too.

--- predictFrostModel.R
+++ predictFrostModel.R
@@ -6,9 +6,18 @@ source('runMCMC_Model.R')
 
 season <- "spring"
 load(paste0('frostModelDataObject_',season,'.RData')) #Loaded as seasonData
-
+ecoregionIDs <- read.table('ecoregionIDs.txt',sep=";",header=TRUE)
 seasonData$N <- nrow(seasonData$D)
 seasonData$regionN <- ncol(seasonData$D)
+ecoIDs <- data.frame(ECO_ID=as.numeric(colnames(seasonData$D)))
+ecoIDs <- left_join(ecoIDs,ecoregionIDs,by='ECO_ID')
+ecoIDs <- ecoIDs %>% dplyr::select(ECO_ID,ECO_NAME)
+ecoIDs$region <- as.character(row_number(ecoIDs$ECO_ID))
+if(season=="spring"){
+  seasonData$DD <- GDD
+}else if(season=="fall"){
+  seasonData$DD <- CDD
+}
 
 # Cumulative precipitation
 #CDD base 20 over a moving window 14 days prior
@@ -28,25 +37,25 @@ model{
   for(j in 1:regionN){
     #Region Random Effects
     b_0[j] ~ dnorm(b_0_mean,b_0_prec)
-    b_GDD[j] ~ dnorm(b_GDD_mean,b_GDD_prec)
+    b_DD[j] ~ dnorm(b_DD_mean,b_DD_prec)
     b_D[j] ~ dnorm(b_D_mean,b_D_prec)
     b_P[j] ~ dnorm(b_P_mean,b_P_prec)
     
     for(i in 1:N) {
-      logit(p[i,j]) <- b_0[j] + b_GDD[j] * GDD[i,j] + b_D[j] * D[i,j] + b_P[j] * P[i,j]
+      logit(p[i,j]) <- b_0[j] + b_DD[j] * DD[i,j] + b_D[j] * D[i,j] + b_P[j] * P[i,j]
       y[i,j] ~ dbern(p[i,j])
     }
   }
   # Priors
   p0 ~ dbeta(1, 1)
   b_0_mean <- logit(p0)
-  b_GDD_mean ~ dunif(-5, 5)
+  b_DD_mean ~ dunif(-5, 5)
   b_D_mean ~ dunif(-5, 5)
   b_P_mean ~ dunif(-5, 5)
   
   #Precisions on Random Effects
   b_0_prec ~ dunif(0.00000001,0.1)
-  b_GDD_prec ~ dunif(0.00000001,0.1)
+  b_DD_prec ~ dunif(0.00000001,0.1)
   b_D_prec ~ dunif(0.00000001,0.1)
   b_P_prec ~ dunif(0.00000001,0.1)
 }
@@ -55,11 +64,54 @@ model{
 j.model <- jags.model(file=textConnection(frostModel),
                       data=seasonData,
                       n.chains=3)
-variableNames <- c('b_0','b_GDD','b_D','b_P','b_0_mean','b_GDD_mean','b_D_mean','b_P_mean','b_0_prec','b_GDD_prec','b_D_prec','b_P_prec')
+variableNames <- c('b_0','b_DD','b_D','b_P','b_0_mean','b_DD_mean','b_D_mean','b_P_mean','b_0_prec','b_DD_prec','b_D_prec','b_P_prec')
 #variableNames <- c('b_0_mean','b_GDD_mean','b_D_mean','b_P_mean')#,'b_0_prec','b_GDD_prec','b_D_prec','b_P_prec')
 #test=coda.samples(j.model,variable.names = variableNames,n.iter=500)
 
-jags.out <- runMCMC_Model(j.model,variableNames = variableNames,baseNum = 30000,iterSize = 5000)
-save(jags.out,file="springFrostPredictionVarburn_hierarchical.RData")
+jags.out <- runMCMC_Model(j.model,variableNames = variableNames,baseNum = 60000,iterSize = 10000)
 
-#jags.mat <- data.frame(as.matrix(jags.out))
+out.mat <- as.matrix(jags.out)
+thinAmount <- round(nrow(out.mat)/5000,digits=0)
+
+jags.out <- window(jags.out,thin=thinAmount)
+
+save(jags.out,file=paste0(season,"FrostPredictionVarburn_hierarchical.RData"))
+
+# jags.mat <- data.frame(data.frame(as.matrix(jags.out)))
+# summary(jags.mat)
+# 
+# b_D_cols = grep("b_D",colnames(jags.mat),fixed=TRUE)
+# b_D <- jags.mat[,b_D_cols]
+# b_D_sum <- data.frame(region=names(b_D),mn=colMeans(b_D),sd=apply(b_D,MARGIN = 2,FUN=sd),driver="D")
+# b_D_sum$region <- c(seq(1,16),"mean","prec")
+# 
+# b_GDD_cols = grep("b_GDD",colnames(jags.mat),fixed=TRUE)
+# b_GDD <- jags.mat[,b_GDD_cols]
+# b_GDD_sum <- data.frame(region=names(b_GDD),mn=colMeans(b_GDD),sd=apply(b_GDD,MARGIN = 2,FUN=sd),driver="GDD")
+# b_GDD_sum$region <- c(seq(1,16),"mean","prec")
+# 
+# allSum <- rbind(b_D_sum,b_GDD_sum)
+# 
+# allSum <- left_join(allSum,ecoIDs,by="region")
+# allSum$ECO_NAME[allSum$region=="mean"] <- "Overall Mean"
+# allSum$ECO_NAME[allSum$region=="prec"] <- "Effect Precision"
+# 
+# 
+# pdf('springFrostOutput.pdf',height=12,width=8)
+# # ggplot(data=b_D_sum,aes(x=region,y=mn)) + 
+# #   geom_bar(stat="identity")+
+# #   geom_errorbar(aes(ymin=mn-sd,ymax=mn+sd),width=0.2,position=position_dodge(0.9))+
+# #   theme_minimal()
+# # ggplot(data=b_GDD_sum,aes(x=region,y=mn)) + 
+# #   geom_bar(stat="identity")+
+# #   geom_errorbar(aes(ymin=mn-sd,ymax=mn+sd),width=0.2,position=position_dodge(0.9))+
+# #   theme_minimal()
+# ggplot(data=allSum,aes(x=ECO_NAME,y=mn,fill=driver)) + 
+#   geom_bar(stat="identity",position=position_dodge())+
+#   geom_errorbar(aes(ymin=mn-sd,ymax=mn+sd),width=0.2,position=position_dodge(0.9))+
+#   theme_minimal() + coord_flip()
+# 
+# dev.off()
+
+
+
